acd6f90f371088b12c0a0c11f9acaed04ef3d601
Chaos Eternal <chaos@shlug.org>
2016-11-23 00:59:43 +0800

    bug fix

--- emacs-ssh-askpass.sh
+++ emacs-ssh-askpass.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
 A=`emacsclient -e '(let ((tmpa ssh-tunnels:custom-password)) (setq ssh-tunnels:custom-password "") tmpa)'`
-#A=`emacsclient -e 'ssh-tunnels:custom-password'`
-bash -c 'echo ${0:1:-1}' $A >> /tmp/xxx
 bash -c 'echo ${0:1:-1}' $A
 
--- emacs-ssh-helper.sh
+++ emacs-ssh-helper.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 #export DISPLAY=:99
 export SSH_ASKPASS=`dirname $0`/emacs-ssh-askpass.sh
-env >> /tmp/xxx
-exec $@ </dev/null &
+bash <(echo $@) </dev/null &
